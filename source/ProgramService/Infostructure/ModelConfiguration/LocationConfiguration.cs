98a5ecd4cd11e5667f92272207bbeffe6f2ab095
haes <haesor@gmail.com>
2026-05-11 12:08:34 +0700

    Добавил конфигурации сущностей

---
+++ source/ProgramService/Infostructure/ModelConfiguration/LocationConfiguration.cs
@@ -0,0 +1,29 @@
+using Domain.LocationContext;
+using Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.LocationContext.ValueObjects;
+
+namespace Infostructure.ModelConfiguration;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.ToTable("Locations");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("Id").HasConversion(x => x.Value, y => LocationId.Create(y));
+        builder.Property(x => x.Name).HasColumnName("Name").HasConversion(x => x.Value, y => NotEmptyName.Create(y));
+        builder.Property(x => x.Address).HasColumnName("Address").HasConversion(x => x.Value, y => LocationAddress.Create(y));
+        builder.Property(x => x.TimeZone).HasColumnName("TimeZone").HasConversion(x => x.Value, y => IanaTimeZone.Create(y));
+        builder.ComplexProperty(x => x.LifeTime, y => {
+            y.Property(a => a.CreatedAt).HasColumnName("CreatedAt");
+            y.Property(a => a.UpdatedAt).HasColumnName("UpdatedAt").IsRequired(false);
+            y.Property(a => a.DeletedAt).HasColumnName("DeletedAt").IsRequired(false);
+        });
+    
+
+        throw new NotImplementedException();
+    }
+    
+}
